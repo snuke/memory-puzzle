8d9bc541b456af7c1ad183b869fd63dbd81c915a
sugim48 <sugim48@gmail.com>
2022-11-08 04:31:46 +0900

    グリッドの色を調整

--- main.js
+++ main.js
@@ -319,7 +319,7 @@ const main = () => {
 
         for (const y of Array(mapH).keys()) {
             for (const x of Array(mapW).keys()) {
-                ctx.strokeStyle = '#404040';
+                ctx.strokeStyle = '#60606080';
                 ctx.lineWidth = 1;
                 ctx.strokeRect(x * D, y * D, D, D);
             }
